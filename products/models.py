05597224b7bcbd57f55c01d7fde01684872d8e31
horrormyth <horrormyth@gmail.com>
2017-06-05 21:58:33 +0300

    Add absolute url for dynamic url handling on templates

--- products/models.py
+++ products/models.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse
 from django.db import models
 
 # Create your models here.
@@ -7,6 +8,8 @@ class Product(models.Model):
     price = models.DecimalField(decimal_places = 2, max_digits = 20 )
     active = models.BooleanField(default=True)
 
-
     def __unicode__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('product_detail', kwargs = {'pk': self.pk})
