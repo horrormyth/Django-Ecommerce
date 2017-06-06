eb9c9bd12d6e95b0ebf7dbd006cd6cf1f975db43
horrormyth <horrormyth@gmail.com>
2017-06-06 20:53:48 +0300

    Add product manager to return active only

--- products/models.py
+++ products/models.py
@@ -2,11 +2,26 @@ from django.core.urlresolvers import reverse
 from django.db import models
 
 # Create your models here.
+
+
+class ProdutQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return ProdutQuerySet(self.model, using = self._db)
+
+    def all(self, *args, **kwargs):
+        return self.get_queryset().active()
+
 class Product(models.Model):
     title = models.CharField(max_length = 512)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places = 2, max_digits = 20 )
     active = models.BooleanField(default=True)
+
+    objects = ProductManager()
 
     def __unicode__(self):
         return self.title
