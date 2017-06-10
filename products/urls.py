a93946db791a95548dcf9be44bb58016c1d26c10
horrormyth <horrormyth@gmail.com>
2017-06-10 13:52:08 +0300

    Display inventory list based on product

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import include, url
 
 
-from .views import ProductDetailView, ProductListView
+from .views import ProductDetailView, ProductListView, VariationListView
 urlpatterns = [
     # Examples:
     # url(r'^$', 'newsletter.views.home', name='home'),
     url(r'^$', ProductListView.as_view(), name = 'products'),
     url(r'^(?P<pk>\d+)/$', ProductDetailView.as_view(), name = 'product_detail'),
+    url(r'^(?P<pk>\d+)/inventory/$', VariationListView.as_view(), name = 'product_inventory'),
+
     # url(r'^(?P<id>\d+)', 'products.views.product_detail_view_function', name = 'product_detail_function')
 ]
 
--- products/views.py
+++ products/views.py
@@ -3,20 +3,42 @@ from django.db.models import Q
 from django.http import Http404
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
-from .models import Product
+from .models import Product,Variation
+
+
+# Editing for admin
+class VariationListView(ListView):
+    model = Variation
+    queryset = Variation.objects.all()
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(VariationListView, self).get_context_data(*args, **kwargs)
+    #     context['now'] = timezone.now()
+    #     context['query'] = self.request.GET.get('q')
+
+    # Get product specific inventory list
+    def get_queryset(self, *args, **kwargs):
+        product_pk =  self.kwargs.get('pk')
+        if product_pk:
+            product = get_object_or_404(Product, pk=product_pk)
+            queryset = Variation.objects.filter(product=product)
+        return queryset
+
+
+
+
 
 
 class ProductListView(ListView):
     model = Product
     queryset = Product.objects.all()
-#     default template name it returns is 'appname/<modelname>_list/detail.html
 
     def get_context_data(self, *args, **kwargs):
-        # overwrite default context to use it further
         context = super(ProductListView, self).get_context_data(*args, **kwargs)
         context['query'] = self.request.GET.get('q')
 
