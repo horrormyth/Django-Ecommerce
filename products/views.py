94dbfe549819f7eaeaebf08affd2620e471ea3ad
horrormyth <horrormyth@gmail.com>
2018-01-07 11:28:18 +0200

    Fix order by issue

--- products/views.py
+++ products/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.contrib import messages
 from django.db.models import Q
 from django.http import Http404
@@ -105,5 +107,5 @@ class ProductDetailView(DetailView):
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         instance = self.get_object()
-        context['related'] = Product.objects.get_related(instance).order_by('?')[:6]
+        context['related'] = sorted(Product.objects.get_related(instance).order_by('?'), key=lambda x: random.random())
         return context
