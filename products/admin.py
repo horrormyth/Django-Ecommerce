ea23964f94d4b2a392c89c294a533c788ead66f9
horrormyth <horrormyth@gmail.com>
2018-01-08 18:37:35 +0200

    Add featured product in the frontpage

--- products/admin.py
+++ products/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from .models import Product, Variation, ProductImage, Category
+from .models import Product, Variation, ProductImage, Category, FeaturedProduct
 
 # Register your models here.
 
 admin.site.register(Product)
 admin.site.register(Variation)
+admin.site.register(FeaturedProduct)
 admin.site.register(ProductImage)
 admin.site.register(Category)
