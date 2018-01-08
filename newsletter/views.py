b64f0353d063071a8eba2a37f71e54aa17f5c5a0
horrormyth <horrormyth@gmail.com>
2018-01-08 21:16:48 +0200

    Perform cleanup

--- newsletter/views.py
+++ newsletter/views.py
@@ -18,31 +18,18 @@ def home(request):
 		"featured_image": featured_image,
 	}
 	if form.is_valid():
-		# form.save()
-		# print request.POST['email'] #not recommended
 		instance = form.save(commit=False)
-
 		full_name = form.cleaned_data.get("full_name")
 		if not full_name:
 			full_name = "New full name"
 		instance.full_name = full_name
-		# if not instance.full_name:
-		# 	instance.full_name = "Justin"
 		instance.save()
 		context = {
 			"title": "Thank you"
 		}
 
 	if request.user.is_authenticated() and request.user.is_staff:
-		# print(SignUp.objects.all())
-		# i = 1
-		# for instance in SignUp.objects.all():
-		# 	print(i)
-		# 	print(instance.full_name)
-		# 	i += 1
-
 		queryset = SignUp.objects.all().order_by('-timestamp')  # .filter(full_name__iexact="Justin")
-		# print(SignUp.objects.all().order_by('-timestamp').filter(full_name__iexact="Justin").count())
 		context = {
 			"queryset": queryset
 		}
@@ -55,13 +42,9 @@ def contact(request):
 	title_align_center = True
 	form = ContactForm(request.POST or None)
 	if form.is_valid():
-		# for key, value in form.cleaned_data.iteritems():
-		# 	print key, value
-		# 	#print form.cleaned_data.get(key)
 		form_email = form.cleaned_data.get("email")
 		form_message = form.cleaned_data.get("message")
 		form_full_name = form.cleaned_data.get("full_name")
-		# print email, message, full_name
 		subject = 'Site contact form'
 		from_email = settings.EMAIL_HOST_USER
 		to_email = [from_email, 'youotheremail@email.com']
