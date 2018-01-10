ba294f2d39e450aa7eac23cf2a415f2f586c3be5
Devndra Ghimire <devndraghimire@Devndras-MacBook-Pro.local>
2018-01-10 10:50:48 +0200

    Add variation and images as inline for product editing

--- products/admin.py
+++ products/admin.py
@@ -2,9 +2,30 @@ from django.contrib import admin
 
 from .models import Product, Variation, ProductImage, Category, FeaturedProduct
 
+
 # Register your models here.
 
-admin.site.register(Product)
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 0
+
+
+class VariationInline(admin.TabularInline):
+    model = Variation
+    extra = 1
+    max_num = 5
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['__unicode__', 'price']
+    inlines = [
+        VariationInline,
+        ProductImageInline,
+    ]
+
+
 admin.site.register(Variation)
 admin.site.register(FeaturedProduct)
 admin.site.register(ProductImage)
