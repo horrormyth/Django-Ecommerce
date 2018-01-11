19394090d559f142467582546761ad0576bad93a
Devndra Ghimire <devndraghimire@Devndras-MacBook-Pro.local>
2018-01-11 12:13:32 +0200

    Update cartview with session authoring

--- carts/views.py
+++ carts/views.py
@@ -9,13 +9,25 @@ from products.models import Variation
 class CartView(View):
 
     def get(self, request, *args, **kwargs):
+        request.session.set_expiry(0)  # until the browser closes
+        cart_id = request.session.get('cart_id', None)
+        if not cart_id:
+            # Create cart
+            cart = Cart()
+            cart.save()
+            cart_id = cart.id
+            request.session['cart_id'] = cart_id
+
+        cart = Cart.objects.get(id=cart_id)
+        if request.user.is_authenticated():
+            cart.user = request.user
+            cart.save()
         item_id = request.GET.get('item', None)
         delete = request.GET.get('delete', False)
         if item_id:
-            item_instance = get_object_or_404(Variation, id=item_id)
+            item = get_object_or_404(Variation, id=item_id)
             quantity = request.GET.get('quantity', 1)
-            cart = Cart.objects.last()
-            cart_item, created = CartItem.objects.get_or_create(cart=cart, item=item_instance)
+            cart_item, created = CartItem.objects.get_or_create(cart=cart, item=item)
             if delete:
                 cart_item.delete()
             else:
