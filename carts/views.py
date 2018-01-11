e7f9556b3a73a59c788cd6a5c298dbb937eeac0c
Devndra Ghimire <horrormyth@gmail.com>
2018-01-11 14:26:10 +0200

    Render cart view with carts view template

--- carts/views.py
+++ carts/views.py
@@ -1,26 +1,33 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django.views.generic.base import View
+from django.views.generic.detail import SingleObjectMixin
 
 from carts.models import CartItem, Cart
 from products.models import Variation
 
 
-class CartView(View):
-    def get(self, request, *args, **kwargs):
-        request.session.set_expiry(0)  # until the browser closes
-        cart_id = request.session.get('cart_id', None)
+class CartView(SingleObjectMixin, View):
+    model = Cart
+    template_name = 'carts/view.html'
+
+    def get_object(self, *args, **kwargs):
+        self.request.session.set_expiry(0)  # until the browser closes
+        cart_id = self.request.session.get('cart_id', None)
         if not cart_id:
             # Create cart
             cart = Cart()
             cart.save()
             cart_id = cart.id
-            request.session['cart_id'] = cart_id
+            self.request.session['cart_id'] = cart_id
 
         cart = Cart.objects.get(id=cart_id)
-        if request.user.is_authenticated():
-            cart.user = request.user
+        if self.request.user.is_authenticated():
+            cart.user = self.request.user
             cart.save()
+        return cart
+
+    def get(self, request, *args, **kwargs):
+        cart = self.get_object()
         item_id = request.GET.get('item', None)
         delete = request.GET.get('delete', False)
         if item_id:
@@ -32,4 +39,9 @@ class CartView(View):
             else:
                 cart_item.quantity = quantity
                 cart_item.save()
-        return HttpResponseRedirect('/')
+
+        context = {
+            'object': cart
+        }
+        template = self.template_name
+        return render(request, template, context)
