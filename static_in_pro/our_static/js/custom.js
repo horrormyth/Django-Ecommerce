fb940de4ef2f6cd87a3e3ae06fad1bd53b661289
horrormyth <horrormyth@gmail.com>
2018-01-13 14:00:27 +0200

    Add message based on the items status

--- static_in_pro/our_static/js/custom.js
+++ static_in_pro/our_static/js/custom.js
@@ -1,9 +1,17 @@
 function showFlashMessage(message) {
-    // var template = "{% include 'alert.html' with message='" + message + "' %}"
     var template = "<div class='container container-alert-flash'><div class='col-sm-3 col-sm-offset-8'><div class='alert alert-success alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" + message + "</div></div></div>"
     $('body').append(template);
     $('.container-alert-flash').fadeIn();
     setTimeout(function () {
         $('.container-alert-flash').fadeOut();
     }, 1500);
+}
+
+
+function checkItemStatus(data) {
+    var flash_message = 'Item succesfully Updated'
+    if (data.item_added) {
+        flash_message = 'Item succesfully added !!'
+    }
+    return flash_message
 }
