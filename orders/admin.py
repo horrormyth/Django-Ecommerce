55384485837afadae3b045cb94f418499a7adffd
horrormyth <horrormyth@gmail.com>
2018-01-16 18:34:52 +0200

    Add order admin and update checkout form

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from orders.models import UserCheckout
+
+admin.site.register(UserCheckout)
--- orders/forms.py
+++ orders/forms.py
@@ -1,14 +1,21 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class GuestCheckoutForm(forms.Form):
     email = forms.EmailField()
     email2 = forms.EmailField(label='Verify Email')
 
-    def validated_email(self):
+    def clean_email2(self):
+        """ Validating email, especially field email2"""
         email = self.cleaned_data.get('email')
         email2 = self.cleaned_data.get('email2')
 
-        if not email == email2:
-            raise forms.ValidationError('Please make sure that provided email are same')
-        return email2
+        if email == email2:
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError('User already exists!! Please login instead. ')
+            return email2
+        else:
+            raise forms.ValidationError('Please make sure provided email are same')
