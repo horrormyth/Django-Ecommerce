66d659d8bf64c57cdfe1ea1e9ed222abfee77a6c
horrormyth <horrormyth@gmail.com>
2018-01-16 15:29:32 +0200

    Register orders app and Create generic model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+
+class GuestCheckout(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, null=True, blank=True)
+    email = models.EmailField()
+
+    def __unicode__(self):
+        return self.email
+
