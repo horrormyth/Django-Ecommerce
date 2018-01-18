af98e2b00ff314d33c37daca6af501ebc22a5ff0
horrormyth <horrormyth@gmail.com>
2018-01-18 15:04:41 +0200

    Add Order model

--- orders/models.py
+++ orders/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 
 # Create your models here.
+from carts.models import Cart
 
 
 class UserCheckout(models.Model):
@@ -29,4 +30,20 @@ class UserAddress(models.Model):
         return '{}, {}'.format(self.street, self.city)
 
 
+class Order(models.Model):
+    cart = models.ForeignKey(Cart)
+    user = models.ForeignKey(UserCheckout)
+    billing_address = models.ForeignKey(UserAddress, related_name='billing_address')
+    shipping_address = models.ForeignKey(UserAddress, related_name='shipping_address')
+    shipping_total_price = models.DecimalField(
+        max_digits=50,
+        decimal_places=2,
+        default=5.99
+    )
+    order_total = models.DecimalField(
+        max_digits=50,
+        decimal_places=2,
+        default=5.99)
 
+    def __unicode__(self):
+        return self.cart.id
