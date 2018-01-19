2200855fb865f3c34c5e74f71150577714ba7b6b
Devndra Ghimire <horrormyth@gmail.com>
2018-01-19 11:45:34 +0200

    Update oder admin to have Order

--- orders/admin.py
+++ orders/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from orders.models import UserCheckout, UserAddress
+from orders.models import UserCheckout, UserAddress, Order
 
+
+admin.site.register(Order)
 admin.site.register(UserAddress)
 admin.site.register(UserCheckout)
