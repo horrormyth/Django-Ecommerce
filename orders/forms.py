40d5aefd700f2c6ea8bacd925f41e8b4a19113e9
Devndra Ghimire <horrormyth@gmail.com>
2018-01-19 14:22:05 +0200

    Add user address create view

--- orders/forms.py
+++ orders/forms.py
@@ -34,3 +34,15 @@ class AddressForm(forms.Form):
         widget=forms.RadioSelect,
         empty_label=None,
     )
+
+
+class UserAddressForm(forms.ModelForm):
+    class Meta:
+        model = UserAddress
+        fields = [
+            'street',
+            'city',
+            'state',
+            'zipcode',
+            'address_type'
+        ]
--- orders/views.py
+++ orders/views.py
@@ -1,17 +1,43 @@
+from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
-from django.views.generic import FormView
+from django.views.generic import FormView, CreateView
 
-from .forms import AddressForm
+from .forms import AddressForm, UserAddressForm
 from .models import UserAddress, UserCheckout
+
+
+class UserAddressCreateView(CreateView):
+    form_class = UserAddressForm
+    template_name = 'forms.html'
+    success_url = '/checkout/address/'
+
+    def get_checkout_user(self):
+        user_checkout_id = self.request.session.get('user_checkout_id')
+        user_checkout = UserCheckout.objects.get(id=user_checkout_id)
+        return user_checkout
+
+    def form_valid(self, form, *args, **kwargs):
+        form.instance.user = self.get_checkout_user()
+        return super(UserAddressCreateView, self).form_valid(form, *args, **kwargs)
 
 
 class AddressSelectFormView(FormView):
     form_class = AddressForm
     template_name = 'orders/address_select.html'
 
-    def get_form(self, *args, **kwargs):
-        form = super(AddressSelectFormView, self).get_form(*args, **kwargs)
+    def dispatch(self, *args, **kwargs):
+        billin_address, shipping_address = self.get_address()
+        if not billin_address:
+            messages.success(self.request, 'Please add billing address to continue')
+            return redirect('create_address')
+        elif not shipping_address:
+            messages.success(self.request, 'Please add shipping address to continue')
+            return redirect('create_address')
+        else:
+            return super(AddressSelectFormView, self).dispatch(*args, **kwargs)
+
+    def get_address(self, *args, **kwargs):
         user_checkout_id = self.request.session.get('user_checkout_id', None)
         user_checkout = UserCheckout.objects.get(id=user_checkout_id)
         bill_address = UserAddress.objects.filter(
@@ -22,14 +48,13 @@ class AddressSelectFormView(FormView):
             user=user_checkout,
             address_type='shipping',
         )
+        return bill_address, ship_address
 
-        if not (bill_address and ship_address):
-            # create a view to add the address
-            redirect('')
-
+    def get_form(self, *args, **kwargs):
+        form = super(AddressSelectFormView, self).get_form(*args, **kwargs)
+        bill_address, ship_address = self.get_address()
         form.fields['billing_address'].queryset = bill_address
         form.fields['shipping_address'].queryset = ship_address
-
         return form
 
     def form_valid(self, form, *args, **kwargs):
