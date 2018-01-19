0a2ba2594fab69f3ebabdb3f3d9b31ef7a9db910
Devndra Ghimire <horrormyth@gmail.com>
2018-01-19 11:07:15 +0200

    Add billing and shipping address to the session

--- orders/views.py
+++ orders/views.py
@@ -22,5 +22,13 @@ class AddressSelectFormView(FormView):
 
         return form
 
+    def form_valid(self, form, *args, **kwargs):
+        billing_address = form.cleaned_data.get('billing_address')
+        shipping_address = form.cleaned_data.get('shipping_address')
+        self.request.session['billing_address_id'] = billing_address.id
+        self.request.session['shipping_address_id'] = shipping_address.id
+        return super(AddressSelectFormView, self).form_valid(form, *args, **kwargs)
+
+
     def get_success_url(self, *args, **kwargs):
         return reverse('checkout')
