d3b28fc4d8a95bc503aeb525b5ca5174f83012de
Devndra Ghimire <horrormyth@gmail.com>
2018-01-19 13:43:36 +0200

    Update addressselect form to get the user from the session

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,9 @@
 from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
 from django.views.generic import FormView
 
-from .models import UserAddress
 from .forms import AddressForm
+from .models import UserAddress, UserCheckout
 
 
 class AddressSelectFormView(FormView):
@@ -11,14 +12,23 @@ class AddressSelectFormView(FormView):
 
     def get_form(self, *args, **kwargs):
         form = super(AddressSelectFormView, self).get_form(*args, **kwargs)
-        form.fields['billing_address'].queryset = UserAddress.objects.filter(
-            user__email=self.request.user.email,
+        user_checkout_id = self.request.session.get('user_checkout_id', None)
+        user_checkout = UserCheckout.objects.get(id=user_checkout_id)
+        bill_address = UserAddress.objects.filter(
+            user=user_checkout,
             address_type='billing',
         )
-        form.fields['shipping_address'].queryset = UserAddress.objects.filter(
-            user__email=self.request.user.email,
+        ship_address = UserAddress.objects.filter(
+            user=user_checkout,
             address_type='shipping',
         )
+
+        if not (bill_address and ship_address):
+            # create a view to add the address
+            redirect('')
+
+        form.fields['billing_address'].queryset = bill_address
+        form.fields['shipping_address'].queryset = ship_address
 
         return form
 
@@ -28,7 +38,6 @@ class AddressSelectFormView(FormView):
         self.request.session['billing_address_id'] = billing_address.id
         self.request.session['shipping_address_id'] = shipping_address.id
         return super(AddressSelectFormView, self).form_valid(form, *args, **kwargs)
-
 
     def get_success_url(self, *args, **kwargs):
         return reverse('checkout')
