3e822c2d9dd8c0793fe057f244c945183155d7a7
Devndra Ghimire <horrormyth@gmail.com>
2018-01-25 11:00:11 +0200

    Update ordermixin to handle enpty cart

--- orders/mixins.py
+++ orders/mixins.py
@@ -5,6 +5,8 @@ from models import Order
 class CartOrderMixin(object):
     def get_order(self, *args, **kwargs):
         cart = self.get_cart()
+        if not cart:
+            return None
         new_order_id = self.request.session.get('order_id', None)
         if not new_order_id:
             new_order = Order.objects.create(cart=cart)
